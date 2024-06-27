a5f4286c2807ac2e38b63668ab56925500259587
Claudio Netto Junior <claudio.netto@sou.unifeob.edu.br>
2024-06-27 14:03:38 -0300

    feat: create base project

---
+++ backend/codigo-certo-fullstack/src/main/java/com/claudionetto/codigo_certo_fullstack/CodigoCertoFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.claudionetto.codigo_certo_fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodigoCertoFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodigoCertoFullstackApplication.class, args);
+	}
+
+}
---
+++ backend/codigo-certo-fullstack/src/test/java/com/claudionetto/codigo_certo_fullstack/CodigoCertoFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claudionetto.codigo_certo_fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodigoCertoFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
