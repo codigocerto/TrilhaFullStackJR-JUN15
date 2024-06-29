e207e992e21ac3b1dd7a16d4964417ed9805de61
Claudio Netto Junior <claudio.netto@sou.unifeob.edu.br>
2024-06-28 21:45:50 -0300

    feat: create service for User

---
+++ backend/codigo-certo-fullstack/src/main/java/com/claudionetto/codigo_certo_fullstack/services/impl/UserServiceImpl.java
@@ -0,0 +1,116 @@
+package com.claudionetto.codigo_certo_fullstack.services.impl;
+
+import com.claudionetto.codigo_certo_fullstack.dtos.mappers.UserMapper;
+import com.claudionetto.codigo_certo_fullstack.dtos.requests.*;
+import com.claudionetto.codigo_certo_fullstack.dtos.responses.UserResponseDTO;
+import com.claudionetto.codigo_certo_fullstack.models.entities.User;
+import com.claudionetto.codigo_certo_fullstack.repositories.UserRepository;
+import com.claudionetto.codigo_certo_fullstack.services.UserService;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserResponseDTO findByID(UUID id) {
+        User user = findByIdOrThrowException(id);
+        return UserMapper.transformEntityToResponse(user);
+    }
+
+    @Override
+    public Page<UserResponseDTO> findAll(Pageable pageable) {
+        Page<User> usersPage = userRepository.findAll(pageable);
+        return usersPage.map(UserMapper::transformEntityToResponse);
+    }
+
+    @Override
+    public UserResponseDTO save(UserRegisterDTO userRegisterDTO) {
+
+        User user = UserMapper.transformRegisterToUser(userRegisterDTO);
+        User userSaved = userRepository.save(user);
+
+        return UserMapper.transformEntityToResponse(userSaved);
+    }
+
+    @Override
+    public UserResponseDTO update(UUID id, UserChangeProfileRequestDTO userChangeProfileRequestDTO) {
+
+        User user = findByIdOrThrowException(id);
+
+        if (userChangeProfileRequestDTO.name() != null){
+            user.setName(userChangeProfileRequestDTO.name());
+        }
+
+        if (userChangeProfileRequestDTO.surname() != null){
+            user.setSurname(userChangeProfileRequestDTO.surname());
+        }
+
+        User userUpdated = userRepository.save(user);
+
+        return UserMapper.transformEntityToResponse(userUpdated);
+    }
+
+    @Override
+    public UserResponseDTO updatePassword(UUID id, UserChangePasswordRequestDTO userChangePasswordRequestDTO) {
+
+        User user = findByIdOrThrowException(id);
+
+        if (!(user.getPassword().equals(userChangePasswordRequestDTO.currentPassword()))){
+            throw new RuntimeException("Password mismatches");
+        }
+
+        user.setPassword(userChangePasswordRequestDTO.newPassword());
+        User userUpdated = userRepository.save(user);
+
+        return UserMapper.transformEntityToResponse(userUpdated);
+    }
+
+    @Override
+    public UserResponseDTO updateEmail(UUID id, UserChangeEmailRequestDTO userChangeEmailRequestDTO) {
+
+        User user = findByIdOrThrowException(id);
+
+        userRepository.findByEmail(userChangeEmailRequestDTO.email()).ifPresent((existingUser) -> {
+            throw new RuntimeException("This email is already in use");
+        });
+
+
+        user.setEmail(userChangeEmailRequestDTO.email());
+        User userUpdated = userRepository.save(user);
+
+        return UserMapper.transformEntityToResponse(user);
+    }
+
+    @Override
+    public UserResponseDTO updateUsername(UUID id, UserChangeUsernameRequestDTO userChangeUsernameRequestDTO) {
+        User user = findByIdOrThrowException(id);
+
+        userRepository.findByUsername(userChangeUsernameRequestDTO.username()).ifPresent((existingUser) -> {
+            throw new RuntimeException("This username is already in use");
+        });
+
+        user.setUsername(userChangeUsernameRequestDTO.username());
+        User userUpdated = userRepository.save(user);
+
+        return UserMapper.transformEntityToResponse(user);
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        findByIdOrThrowException(id);
+        userRepository.deleteById(id);
+    }
+
+    private User findByIdOrThrowException(UUID id){
+        return userRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+    }
+}
