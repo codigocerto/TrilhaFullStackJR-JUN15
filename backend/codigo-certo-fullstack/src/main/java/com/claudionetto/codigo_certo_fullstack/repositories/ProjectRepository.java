77f09552fecfadb567070ee3c0a27a32a06a66bb
Claudio Netto Junior <claudio.netto@sou.unifeob.edu.br>
2024-06-28 21:23:51 -0300

    feat: create repository for User

--- backend/codigo-certo-fullstack/src/main/java/com/claudionetto/codigo_certo_fullstack/repositories/ProjectRepository.java
+++ backend/codigo-certo-fullstack/src/main/java/com/claudionetto/codigo_certo_fullstack/repositories/ProjectRepository.java
@@ -1,8 +1,11 @@
 package com.claudionetto.codigo_certo_fullstack.repositories;
 
 import com.claudionetto.codigo_certo_fullstack.models.entities.Project;
+import com.claudionetto.codigo_certo_fullstack.models.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
--- backend/codigo-certo-fullstack/src/main/java/com/claudionetto/codigo_certo_fullstack/repositories/UserRepository.java
+++ backend/codigo-certo-fullstack/src/main/java/com/claudionetto/codigo_certo_fullstack/repositories/UserRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findById(UUID id);
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
 }
