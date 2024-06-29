c63e982ae04e7a229287d5c3f5733c057ba3553c
Claudio Netto Junior <claudio.netto@sou.unifeob.edu.br>
2024-06-28 21:18:09 -0300

    feat: create repository for User

---
+++ backend/codigo-certo-fullstack/src/main/java/com/claudionetto/codigo_certo_fullstack/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.claudionetto.codigo_certo_fullstack.repositories;
+
+import com.claudionetto.codigo_certo_fullstack.models.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findById(UUID id);
+}
