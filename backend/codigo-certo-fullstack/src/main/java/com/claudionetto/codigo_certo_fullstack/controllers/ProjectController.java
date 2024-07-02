4b458fa59f65b78611fa99163b24e858debd5c9e
Claudio Netto Junior <claudio.netto@sou.unifeob.edu.br>
2024-07-02 17:58:41 -0300

    refactor: remove methods post, put and delete for ProjectController

--- backend/codigo-certo-fullstack/src/main/java/com/claudionetto/codigo_certo_fullstack/controllers/ProjectController.java
+++ backend/codigo-certo-fullstack/src/main/java/com/claudionetto/codigo_certo_fullstack/controllers/ProjectController.java
@@ -1,16 +1,15 @@
 package com.claudionetto.codigo_certo_fullstack.controllers;
 
-import com.claudionetto.codigo_certo_fullstack.dtos.requests.ProjectRequestDTO;
 import com.claudionetto.codigo_certo_fullstack.dtos.responses.ProjectResponseDTO;
 import com.claudionetto.codigo_certo_fullstack.services.ProjectService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.net.URI;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/projects")
@@ -32,26 +31,4 @@ public class ProjectController {
 
         return ResponseEntity.ok(project);
     }
-
-    @PostMapping
-    public ResponseEntity<Void> save(@RequestBody @Valid ProjectRequestDTO projectRequestDTO){
-        ProjectResponseDTO project = projectService.save(projectRequestDTO);
-
-        return ResponseEntity.created(URI.create("/projects/" + project.id())).build();
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<ProjectResponseDTO> update(@PathVariable(name = "id") Long id,
-                                                     @RequestBody @Valid ProjectRequestDTO projectRequestDTO){
-
-        ProjectResponseDTO projectResponseDTO = projectService.update(id, projectRequestDTO);
-        return ResponseEntity.ok(projectResponseDTO);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteById(@PathVariable(name = "id") Long id){
-        projectService.deleteById(id);
-        return ResponseEntity.noContent().build();
-    }
-
 }
