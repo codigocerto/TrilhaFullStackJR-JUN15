e55588ed510efc76a098c518970e67e809d21f62
Claudio Netto Junior <claudio.netto@sou.unifeob.edu.br>
2024-07-03 18:53:19 -0300

    refactor: add new attributes for dtos

--- backend/codigo-certo-fullstack/src/main/java/com/claudionetto/codigo_certo_fullstack/dtos/mappers/ProjectMapper.java
+++ backend/codigo-certo-fullstack/src/main/java/com/claudionetto/codigo_certo_fullstack/dtos/mappers/ProjectMapper.java
@@ -2,16 +2,26 @@ package com.claudionetto.codigo_certo_fullstack.dtos.mappers;
 
 import com.claudionetto.codigo_certo_fullstack.dtos.requests.ProjectRequestDTO;
 import com.claudionetto.codigo_certo_fullstack.dtos.responses.ProjectResponseDTO;
+import com.claudionetto.codigo_certo_fullstack.dtos.responses.ProjectUserResponseDTO;
 import com.claudionetto.codigo_certo_fullstack.models.entities.Project;
+import com.claudionetto.codigo_certo_fullstack.models.entities.User;
 
 public class ProjectMapper {
 
-    public static Project transformRequestToEntity(ProjectRequestDTO projectRequestDTO){
+    public static Project transformRequestToEntity(ProjectRequestDTO projectRequestDTO, User user){
 
         return Project.builder()
                 .name(projectRequestDTO.name())
                 .description(projectRequestDTO.description())
                 .projectStatus(projectRequestDTO.projectStatus())
+                .user(user)
+                .build();
+    }
+
+    public static ProjectUserResponseDTO transformUserEntityToProjectUserResponseDTO(User user){
+        return ProjectUserResponseDTO.builder()
+                .username(user.getUsername())
+                .userId(user.getId())
                 .build();
     }
 
@@ -21,6 +31,7 @@ public class ProjectMapper {
                 .name(project.getName())
                 .description(project.getDescription())
                 .projectStatus(project.getProjectStatus())
+                .projectUserResponse(transformUserEntityToProjectUserResponseDTO(project.getUser()))
                 .build();
     }
 
